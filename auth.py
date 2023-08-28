c5e002093f0dee0db154d7dbeb8c28a22e17a2f2
ASPP Student <student@localhost>
2023-08-28 15:24:10 +0300

    Creates automatically new users

--- auth.py
+++ auth.py
@@ -17,16 +17,19 @@ def write_pwdb(pwdb):
     with open(PATH, "w") as f:
         json.dump(pwdb, f)
 
-
 def authenticate(username, password, pwdb):
     if username in pwdb:
         if password == pwdb[username]:
             print("Successfully authenticated!")
         else:
-            print("Wrong Password")
+            print("Authentication failed :(")
     else:
-        pwdb = add_user(username, password, pwdb)
-        write_pwdb(pwdb)
+        print("Non-existent user, create new one?")
+        answer = input("y or n:")
+        if answer == "y":
+            pwdb = add_user(username, password, pwdb)
+            write_pwdb(pwdb)
+            print("User created")
 
 def add_user(username, password, pwdb):
     pwdb[username] = password
@@ -35,4 +38,3 @@ def add_user(username, password, pwdb):
 username, password = get_credentials()
 pwdb = read_pwdb()
 authenticate(username, password, pwdb)
-
